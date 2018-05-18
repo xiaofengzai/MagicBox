10598350ba7aea48b6d2c27e995176550f49f672
feng.wen <feng.wen@sao.so>
2018-05-18 14:35:12 +0800

    修改bug

--- collection.py
+++ collection.py
@@ -1,2 +1,29 @@
 def difference(list1,list2):
-  return list(set(list1).difference(set(list2)))
+    return list(set(list1).difference(set(list2)))
+  
+def ascDifference(list1,list2):
+    return sorted(list(set(list1).difference(set(list2))))
+  
+def descDifference(list1,list2):
+    return sorted( list(set(list1).difference(set(list2))),reverse=True)
+  
+def union(list1,list2):
+    return list(set(list1) |(set(list2)))
+  
+def ascUnion(list1,list2):
+    return sorted(list(set(list1) |(set(list2))))
+  
+def descUnion(list1,list2):
+    return sorted(list(set(list1) |(set(list2))),reverse=True)
+  
+def intersection(list1,list2):
+    return list(set(list1) & (set(list2)))
+  
+def ascIntersection(list1,list2):
+    return sorted(list(set(list1) & (set(list2))))
+  
+def descIntersection(list1,list2):
+    return sorted(list(set(list1) & (set(list2))),reverse=True) 
+  
+  
+  
